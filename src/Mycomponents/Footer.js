a19e5e672f04b62c5da9ef3f6fc910fbf355ba07
vrmaverick <119831329+vrmaverick@users.noreply.github.com>
2023-02-02 16:15:25 +0530

    Add files via upload

---
+++ src/Mycomponents/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './footer.css'
+
+const Footer = () => {
+  return (
+
+    <footer id="footer">ALL  RIGHTS  RESERVED &copy;</footer>
+  )
+}
+
+export default Footer
